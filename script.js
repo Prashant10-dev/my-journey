dff3d8c47b07b60b15dd6575f454eab7cf4bd40b
Prashant10-dev <gautamk8749@gmail.com>
2026-08-13 17:45:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -242,13 +242,13 @@ document.addEventListener("DOMContentLoaded", () => {
       card.innerHTML = `
 
         <div class="media-wrapper photo-wrapper">
-<img
-  src="${escapeHTML(file.download_url)}"
-  alt="${escapeHTML(title)}"
-  loading="lazy"
-  decoding="async"
->
-         
+
+          <img
+            src="${escapeHTML(file.download_url)}"
+            alt="${escapeHTML(title)}"
+            loading="lazy"
+          >
+
           <div class="photo-overlay">
             <span>⛶</span>
             <small>View</small>
@@ -1089,3 +1089,4 @@ document.addEventListener("DOMContentLoaded", () => {
   loadGallery();
 
 });
+       
